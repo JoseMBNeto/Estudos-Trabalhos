1dc36dda684affb4e05b8d0843eb8b6dc91d5479
José Milton <miltoneto4231@gmail.com>
2026-03-31 21:23:33 -0300

    subvetor_vogal

---
+++ PIF/estudos_solo/string/subvetor_vogal.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+char* maiorSubstringVogais(char *S, int *maior) {
+    int atual = 0; 
+    *maior = 0;  
+
+    char *p = S;
+    char *indice_atual = S;
+    char *indice_maior = S;
+
+    while(*p != '\0'){
+            if(*p == 'a' || *p == 'e' || *p == 'i' ||*p == 'o' ||*p == 'u'){
+                if(atual == 0){
+                    indice_atual = p;
+                }
+
+                atual++;
+
+                if(atual > *maior){
+                    *maior = atual;
+                    indice_maior = indice_atual;
+                }
+            } else{
+                atual = 0;
+            }
+
+        p++;
+    }
+    return indice_maior; 
+}
+
+char *copiar(char *saida, char *S, int n) {
+    char *inicio_saida = saida;
+
+    for (int i = 0; i < n; i++) {
+        *saida = *S;
+        saida++;
+        S++;
+    }
+    *saida = '\0';
+
+    return inicio_saida;
+}
+
+
+int main(void) {
+
+    char str[100];
+    char saida[100];
+
+    fgets(str,99,stdin);
+
+    int tamanho; 
+    char *inicio = maiorSubstringVogais(str, &tamanho);
+
+    printf("\nA maior substring de vogais tem [%d] vogais\n",tamanho);
+
+    copiar(saida,inicio, tamanho);
+    printf("\n%s\n",saida);
+
+    return 0;
+
+}
