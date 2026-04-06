03adf4c965437d38940a3f503a22d155bbe852d7
José Milton <miltoneto4231@gmail.com>
2026-04-06 17:21:57 -0300

    q5

---
+++ PIF/estudos_solo/alocacao_dinamica/fichamonitoria/qp6.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    
+    int n;
+    scanf("%d", &n);
+
+    int *vet = (int *) malloc (n * sizeof(int));
+
+    for (int i=0;i<n;i++){
+        scanf("%d", &vet[i]);
+    }
+
+    int x, p;
+
+    printf("Digite X :");
+    scanf("%d", &x);
+
+    printf("Digite p :");
+    scanf("%d", &p);
+
+    vet = realloc (vet, (n + 1) * sizeof(int));
+
+    for (int i=n;i>p;i--){
+        vet[i] = vet[i-1];
+    }
+    vet[p] = x;
+    n++;
+
+    for (int i=0;i<n;i++){
+        printf("%d", vet[i]);
+    }
+
+
+    return 0;
+}
