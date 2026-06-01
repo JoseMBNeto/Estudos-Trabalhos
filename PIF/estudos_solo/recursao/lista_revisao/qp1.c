3979a07f174e22cb3280e162b690b3ee3c937e67
José Milton <miltoneto4231@gmail.com>
2026-06-01 15:03:34 -0300

    qp1

--- PIF/estudos_solo/recursao/lista_revisao/qp1.c
+++ PIF/estudos_solo/recursao/lista_revisao/qp1.c
@@ -1,3 +1,11 @@
+/*
+Implemente recursivamente a função int somaDigitos(int n) que recebe um inteiro não
+negativo e retorna a soma de seus dígitos. Não use laços.
+somaDigitos(123) → 6
+somaDigitos(9) → 9
+somaDigitos(0) → 0
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -21,3 +29,4 @@ int main() {
     
     return 0;
 }
+
