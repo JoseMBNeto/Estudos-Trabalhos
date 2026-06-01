28e73944494bfdd3d4834893d06940fd4cdfc63d
José Milton <miltoneto4231@gmail.com>
2026-06-01 20:07:30 -0300

    limpando arquivos errados

--- Trabalhos/Projeto-FDS-2026.1/projeto-fds-2026-1/monitor/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Sistema, Incidente
-
-admin.site.register(Sistema)
-admin.site.register(Incidente)
--- Trabalhos/Projeto-IHC-2026.1/README.md
+++
@@ -1,3 +0,0 @@
-# Projeto-IHC-2026.1
-
-- Projeto desenvolvido para criar uma interface de um banco digital para pessoas com deficiência motora 
